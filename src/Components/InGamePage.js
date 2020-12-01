9d348fe500f175b83deeef9f09ecedc391e9b02d
Olivier DEWALQUE <olivier.dewalque@student.vinci.be>
2020-12-01 14:14:49 +0100

    finalisation single player + animejs

--- src/Components/InGamePage.js
+++ src/Components/InGamePage.js
@@ -26,15 +26,16 @@ let inGamePage = `
         <div id="round"></div>
         <div id="image"></div>
         <div id="bottomDash"></div>
-        <div id="answerForm"></div>
         <div id="state"></div>
+        <div id="answerForm"></div>
       </div><!-- div id=secondSquare -->    
     </div><!-- div id=firstSquare -->
 </div><!-- div id=centerPage -->`;
 
 let page = document.querySelector("#page");
 let actualRound = 1;
-let myVar;
+let myVarForTimer;
+let imagesToDisplay = new Array(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13);
 let imagesAlreadyDisplayed = new Array();
 let correctAnswers = 0;
 let endGamePage;
@@ -50,170 +51,154 @@ const InGamePage = () => {
 const onGameSettings = (data) => {
   if (!data) return;
   document.getElementById("state").innerHTML = ``;//On remet l'état à "zéro"
-  clearInterval(myVar);//Je clear l'interval pour éviter qu'il y ait 2 timers lorsqu'une réponse est correcte
-  myVar = setInterval(myTimer, 1000);
+  clearInterval(myVarForTimer);//Je clear l'interval pour éviter qu'il y ait 2 timers lorsqu'une réponse est correcte
+  myVarForTimer = setInterval(myTimer, 1000);
 
   let nbRound = data.nbRound;
   document.getElementById("round").innerHTML = `<h1>Round : ${actualRound}/${nbRound}</h1>`;
-  if(actualRound > nbRound){
-    //clearInterval(myVar);
+  if(actualRound > nbRound){ //Quand la partie est finie
     isGameFinished = true;
     onEndGame(data.nbRound);
     console.log("gameIsFinished");
   }
+  
   let timer = data.roundTime;
   document.getElementById("timer").innerHTML = `<h1>${timer}</h1>`;//Je le mets avant la fonction pour qu'il soit mis dans l'html
-  function myTimer() {
-    document.getElementById("timer").innerHTML = `<h1>${timer}</h1>`;
+  function myTimer() { //Fonction pour actualiser le timer à chaque seconde
+    document.getElementById("timer").innerHTML = `<h1>${timer}</h1>`; 
     timer--;
-    if(timer < 0){
+    if(timer < 0){ //Si le timer est écoulé
       onCallGame();
       actualRound++;
       console.log("Temps écoulé");
-      clearInterval(myVar);
+      clearInterval(myVarForTimer);
     }
   }
-
   onCallImage();
 };
 
-
+//Récupère l'image à afficher via un appel API mis dans la const onCallImage()
 const onGetImage = (data) => {
   if (!data) return;
   //Sert à savoir si une id a déjà été utilisée
-  //Fonctionne mais ca serait mieux de gérer ça dans le backend
-  /*if(imagesAlreadyDisplayed.includes(data.id)){
+  //Fonctionne mais ca serait mieux de gérer ça dans le backend(Mais c'est plus compliqué)
+  if(imagesAlreadyDisplayed.includes(data.id)){
+    console.log("Image déjà affichée :",data.wordToFind);
     onCallImage();
   }
-  else{*/
-    if(data.id === 1){
-      document.getElementById("image").innerHTML = `<img style="width:50%;length:50%" src="${p1}" alt="${data.id}">`;
-    }else if(data.id === 2){
-      document.getElementById("image").innerHTML = `<img style="width:50%;length:50%" src="${p2}" alt="${data.id}">`;
-    }else if(data.id === 3){
-      document.getElementById("image").innerHTML = `<img style="width:50%;length:50%" src="${p3}" alt="${data.id}">`;
-    }else if(data.id === 4){
-      document.getElementById("image").innerHTML = `<img style="width:50%;length:50%" src="${p4}" alt="${data.id}">`;
-    }else if(data.id === 5){
-      document.getElementById("image").innerHTML = `<img style="width:50%;length:50%" src="${p5}" alt="${data.id}">`;
-    }else if(data.id === 6){
-      document.getElementById("image").innerHTML = `<img style="width:50%;length:50%" src="${p6}" alt="${data.id}">`;
-    }else if(data.id === 7){
-      document.getElementById("image").innerHTML = `<img style="width:50%;length:50%" src="${p7}" alt="${data.id}">`;
-    }else if(data.id === 8){
-      document.getElementById("image").innerHTML = `<img style="width:50%;length:50%" src="${p8}" alt="${data.id}">`;
-    }else if(data.id === 9){
-      document.getElementById("image").innerHTML = `<img style="width:50%;length:50%" src="${p9}" alt="${data.id}">`;
-    }else if(data.id === 10){
-      document.getElementById("image").innerHTML = `<img style="width:50%;length:50%" src="${p10}" alt="${data.id}">`;
-    }else if(data.id === 11){
-      document.getElementById("image").innerHTML = `<img style="width:50%;length:50%" src="${p11}" alt="${data.id}">`;
-    }else if(data.id === 12){
-      document.getElementById("image").innerHTML = `<img style="width:50%;length:50%" src="${p12}" alt="${data.id}">`;
-    }else if(data.id === 13){
-      document.getElementById("image").innerHTML = `<img style="width:50%;length:50%" src="${p13}" alt="${data.id}">`;
+  else{
+    document.getElementById("image").innerHTML = `<img style="width:50%" id="displayedImage" src="${imagesToDisplay[data.id-1]}" alt="${data.id}">`;
+    imagesAlreadyDisplayed.push(data.id);
+    
+    //Gère le zoom et le dezoom de l'image
+    //Serait mieux de gérer ça en dehors de la const onGetImage()
+    document.getElementById("displayedImage").addEventListener('mouseleave', () =>{
+      document.getElementById("displayedImage").style.width = "50%";
+      console.log("Dezoom");
+    });
+    document.getElementById("displayedImage").addEventListener('mouseenter', () =>{
+      document.getElementById("displayedImage").style.width = "70%";
+      console.log("Zoom");
+    });
+
+    let bottomDash = `<h1><span>`;  
+    for(let i=0; i<data.wordToFind.length; i++){
+      bottomDash += ` _ `;
     }
-  //}
+    bottomDash += `</span></h1>`;
 
-  imagesAlreadyDisplayed.push(data.id);
+    document.getElementById("bottomDash").innerHTML = bottomDash;
 
-  let bottomDash = `<h1><span>`;  
-  for(let i=0; i<data.wordToFind.length; i++){
-    bottomDash += ` _ `;
+    onCheckAnswer(data);
   }
-  bottomDash += `</span></h1>`;
+};
 
-  document.getElementById("bottomDash").innerHTML = bottomDash;
+const onCheckAnswer = (data) => {
+  document.getElementById("answerForm").innerHTML =`
+  <form>
+    <input class="form-control" type="text" name="answer" id="answer" placeholder="Enter answer" autocomplete="off"/>
+    <input class="buttonHP" type="submit" value="Submit answer" />
+  </form>`;
 
-  onCheckAnswer(data);
-  
-  };
+  document.getElementById("answer").focus();//Pour que le curseur aille directement dans le formulaire
+  let answerForm = document.querySelector("form");
 
-  const onCheckAnswer = (data) => {
-    document.getElementById("answerForm").innerHTML =`
-    <form>
-      <input class="form-control" type="text" name="answer" id="answer" placeholder="Enter answer"/>
-      <input class="buttonHP" type="submit" value="Submit answer" />
-    </form>`;
-
-    document.getElementById("answer").focus();//Pour que le curseur aille directement dans le formulaire
-    let answerForm = document.querySelector("form");
-
-    answerForm.addEventListener("submit",(event) =>{
+  answerForm.addEventListener("submit",(event) =>{
     event.preventDefault();
     let answer = answerForm.elements[0].value;
       
     if(answer === data.wordToFind){
-      document.getElementById("state").innerHTML = `<h1>Bonne réponse !</h1>`;
+      document.getElementById("state").innerHTML = `<h1 style="color:green">Right answer !</h1>`;
       console.log("Bien joué le mot était", data.wordToFind);
       actualRound++;
       correctAnswers++;
-      //setTimeout(onCallGame,1000);
-      onCallGame();
+      setTimeout(onCallGame,1000); //Pour afficher pdt 1 sec qu'on a trouvé la bonne rep
+      //onCallGame();
     }else{
-      document.getElementById("state").innerHTML = `<h1>Mauvaise réponse !</h1>`;
+      answerForm.elements[0].value = ``; //Reset l'input lorsque l'utilisateur à entre une mauvaise rep
+      document.getElementById("state").innerHTML = `<h1 style="color:red">Wrong answer !</h1>`;
     }
     console.log("Réponse : " + answer);
   });
-    if(isGameFinished){
-      clearInterval(myVar);
-      page.innerHTML = endGamePage;
-    }
+  if(isGameFinished){ //Si la partie est finie, on remplace la inGamePage par l'html présent dans la const onEndGame()
+    clearInterval(myVarForTimer);
+    page.innerHTML = endGamePage;
   }
+}
 
-  const onEndGame = (nbRound) => {
-    console.log(correctAnswers + "/" + nbRound);
-    endGamePage = 
-    `<div id="centerPage">
-      <img id="logo2" src="${logo}" alt="logo GuessIt">
-      <h1>Guess It</h1>
-      <h4>Multiplayer game</h4>
-      <div id="firstSquare">
-        <div id="secondSquare">
-          <h1>Game Finished</h1>
-          <h1>${correctAnswers}/${nbRound} Correct answers</h1>
-        </div><!-- div id=secondSquare -->    
-      </div><!-- div id=firstSquare -->
-    </div><!-- div id=centerPage -->`;
-  }
+const onEndGame = (nbRound) => {
+  console.log(correctAnswers + "/" + nbRound);
+  endGamePage = 
+  `<div id="centerPage">
+    <img id="logo2" src="${logo}" alt="logo GuessIt">
+    <h1>Guess It</h1>
+    <h4>Multiplayer game</h4>
+    <div id="firstSquare">
+      <div id="secondSquare">
+        <h1>Game Finished</h1>
+        <h1>${correctAnswers}/${nbRound} Correct answers</h1>
+      </div><!-- div id=secondSquare -->    
+    </div><!-- div id=firstSquare -->
+  </div><!-- div id=centerPage -->`;0
+}
 
-  const onError = (err) => {
-    let errorMessage = err.message;
-    RedirectUrl("/error", errorMessage);
-  };
+const onError = (err) => {
+  let errorMessage = err.message;
+  RedirectUrl("/error", errorMessage);
+};
 
-  const onCallGame = () => {
-    fetch("/api/games", {
-      method: "GET",
-      headers: {
-      },
+const onCallGame = () => {
+  fetch("/api/games", {
+    method: "GET",
+    headers: {
+    },
+  })
+    .then((response) => {
+      if (!response.ok)
+        throw new Error(
+          "Error code : " + response.status + " : " + response.statusText
+        );
+      return response.json();
     })
-      .then((response) => {
-        if (!response.ok)
-          throw new Error(
-            "Error code : " + response.status + " : " + response.statusText
-          );
-        return response.json();
-      })
-      .then((data) => onGameSettings(data))
-      .catch((err) => onError(err));
-  }
+    .then((data) => onGameSettings(data))
+    .catch((err) => onError(err));
+}
 
-  const onCallImage = () => {
-    fetch("/api/images", {
-      method: "GET",
-      headers: {
-      },
+const onCallImage = () => {
+  fetch("/api/images", {
+    method: "GET",
+    headers: {
+    },
+  })
+    .then((response) => {
+      if (!response.ok)
+        throw new Error(
+          "Error code : " + response.status + " : " + response.statusText
+        );
+      return response.json();
     })
-      .then((response) => {
-        if (!response.ok)
-          throw new Error(
-            "Error code : " + response.status + " : " + response.statusText
-          );
-        return response.json();
-      })
-      .then((data) => onGetImage(data))
-      .catch((err) => onError(err));
-  }
+    .then((data) => onGetImage(data))
+    .catch((err) => onError(err));
+}
 
 export default InGamePage;
