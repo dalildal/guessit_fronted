d5fc63b7be9a67611d317890e36dbcdd8aecfbae
Christian GAVIRIA <christian.gaviria@student.vinci.be>
2020-12-04 15:23:13 +0100

    socket client side

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,7 +1,7 @@
 //import { setLayout } from "../utils/render.js";
 import logo from "../images/guessItLogo.png";
 import { RedirectUrl } from "./Router.js";
-
+import { setLayout } from "../utils/render";
 
 let homePage = `
 
@@ -11,15 +11,21 @@ let homePage = `
   <h4>Multiplayer game</h4>
   <div id="firstPartHomePage">
     <div id="secondPartHomePage">
+      <div id="createNameForm">
+        <form>
+          <input class="form-control" type="text" name="name" id="name" placeholder="pseudo"/>
+          <input class="buttonHP" type="submit" value="Create Name" />
+        </form>
+      </div>
       <div id="createGameForm">
-        <form action="/chatRoom">
-          <input class="form-control" type="text" name="pseudo" id="pseudo" placeholder="pseudo"/>
+        <form action="/createGame">
+        <input class="form-control" type="text" name="pseudo" id="pseudo" placeholder="pseudo"/>
           <input class="buttonHP" type="submit" value="Create Game" />
         </form>
       </div>
       <div id="joinGameForm">
-        <form>
-          <input class="form-control" type="text" name="link" id="link" placeholder="Invitation Link"/>
+        <form action="/chatRoom">
+          <input class="form-control" type="text" name="pseudo" id="pseudo" placeholder="pseudo"/>
           <input class="buttonHP" type="submit" value="Join Game" />
         </form>
       </div>
@@ -31,14 +37,18 @@ let homePage = `
 let page = document.querySelector("#page");
 
 const HomePage = async () => {
-
+  
   page.innerHTML = homePage;
 
+
+  // let createNameForm = document.getElementById("createNameForm");
+  // createNameForm.addEventListener("submit", onCreateName);
   // let createGameForm = document.getElementById("createGameForm");
   // createGameForm.addEventListener("submit", onCreateGame);
-  //let joinGameForm = document.getElementsById("joinGameForm");
-  //joinGameForm.addEventListener("submit", onJoinGame);
+  // let joinGameForm = document.getElementById("joinGameForm");
+  // joinGameForm.addEventListener("submit", onJoinGame);
 };
+
 
 const onCreateGame = (e) => {
   e.preventDefault();
