91498de6b43f871c4e6600f08fb1d6baf7a9e79a
Olivier DEWALQUE <olivier.dewalque@student.vinci.be>
2020-12-10 16:07:57 +0100

    integration changement dalil

--- src/Components/InGamePage.js
+++ src/Components/InGamePage.js
@@ -22,39 +22,46 @@ const { pseudo } = qs.parse(location.search, {
 });
 
 let inGamePage = `
-<div class="chat-container">
-  <header class="chat-header">
-    <h1><i class="fas fa-smile"></i> GuessIt</h1>
-    <a href="index.html" class="btn">Leave Room</a>
-  </header>
-  <main class="chat-main">
-    <div class="chat-sidebar">
-      <h3><i class="fas fa-comments"></i> Room Name:</h3>
-      <h2 id="room-name">JavaScript</h2>
-      <h3><i class="fas fa-users"></i> Users</h3>
-      <ul id="users"></ul>
+<div class="logo-ingame">
+    <div class="container">
+        <div class="squares-ingame"></div>
+        <div class="squares-ingame"></div>
     </div>
-    <div id="centerPage">
-    <div id="waiting"></div>
-    <div id="firstSquare">
-    <div id="secondSquare">
-      <div id="timer"></div>
-      <div id="round"></div>
-      <div id="image"></div>
-      <div id="bottomDash"></div>
-      <div id="state"></div>
-      <div id="answerForm"></div>
-    </div><!-- div id=secondSquare -->    
-  </div><!-- div id=firstSquare -->
-  </div><!-- div id=centerPage -->
-    <div class="chat-messages"></div>
-  </main>
-  <div class="chat-form-container">
-    <form id="chat-form">
-      <input id="msg" type="text" placeholder="Enter Message" required autocomplete="off"/>
-      <button class="btn"><i class="fas fa-paper-plane"></i> Send</button>
-    </form>
-  </div>
+    <div class="container">
+        <div class="squares-ingame"></div>
+        <div class="squares-ingame"></div>
+    </div>
+</div>
+
+<div class="container-game">
+    <div class="container-menu">
+        <h2 id="users-title">USERS</h2>
+        <br>
+        <ul id="users"></ul>
+    </div>
+    <div class="container-ingame">
+        <div id="round"></div>
+        <div id="centerPage">
+            <div id="waiting"></div>
+            <div id="hey"></div>
+            <div id="image"></div>
+            <div id="bottomDash"></div>
+            <div id="state"></div>
+            <div id="answerForm"></div>
+        </div><!-- div id=centerPage -->
+        <div class="container-timer">
+            <h1 id="timer"></h1>
+        </div>
+    </div>
+    <div class="container-chat">
+        <div class="chat-messages"></div>
+        <div class="chat-form">
+            <form id="chat-form">
+                <input id="msg" type="text" placeholder="Enter Message" required autocomplete="off" />
+                <!-- <button class="btn"><i class="fas fa-paper-plane"></i> Send</button> -->
+            </form>
+        </div>
+    </div>
 </div>`;
 
 let page = document.querySelector("#page");
@@ -289,8 +296,6 @@ const onEndGame = (users) => {
       `</div><!-- div id=secondSquare -->    
     </div><!-- div id=firstSquare -->
   </div><!-- div id=centerPage -->`;
-
-
 
 }
 
